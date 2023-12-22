3fa84341a1ce075b81203b891fad7b745ea01106
James Wallace <sausages@gail.com>
2023-12-21 21:23:21 -0500

    One last file for now.

--- main.php
+++ main.php
@@ -8,6 +8,7 @@ include 'map.inc';
 include 'player.inc';
 include 'sky.inc';
 include 'plant.inc';
+include 'wetable.inc';
 
 $skyHeight = 10;
 $groundHeight = 15;
@@ -18,38 +19,4 @@ $maxMinerals = 15;
 
 $p = new Player($skyHeight, 20);
 $map = new Map($skyHeight, $groundHeight, $vpWidth, $mapWidth, $maxRain, $maxMinerals, $p);
-echo $map;
-
-trait Alphabet {
-	public function newFunction() {
-		echo "hello";
-	}
-}
-
-trait Wetable {
-	public $wetness;
-
-	public function setWetness($w) {
-		$this->wetness = $w;
-	}
-	
-	public function getWetness() {
-		return $this->wetness;
-	}
-
-	public function incrWetness() {
-		return $this->wetness++;
-	}
-
-	public function decrWetness() {
-		if ($this->wetness > 0) {
-			return $this->wetness--;
-		}
-
-		return $this->wetness;
-	}
-
-	public function isWet() {
-		return $this->wetness > 0;
-	}
-}
+?>
