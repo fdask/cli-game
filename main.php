18ed552ad12d43cdd1116b10ad17dc7e8683fc71
James Wallace <sausages@gmail.com>
2023-12-31 11:17:16 -0500

    feat!: Use percentage based maximums (#22)
    * feat: rocks are now added as a percentage

--- main.php
+++ main.php
@@ -16,78 +16,13 @@ include 'config.inc';
 include 'level.inc';
 include 'objective.inc';
 include 'portal.inc';
+include 'levelGenerator.inc';
 
-if (!(file_exists("level1.dat"))) {
-    // generate a default level 
-    $ret = array(
-        'mapWidth' => 60,
-        'mapHeight' => 22,
-        'vpWidth' => 30,
-        'skyHeight' => 8,
-        'enableRocks' => true,
-        'maxRocks' => 15,
-        'rocksAtStart' => false,
-        'enableRain' => true,
-        'maxRain' => 7,
-        'rainStrengthMax' => 9,
-        'rainStrengthMin' => 7,
-        'rainAtStart' => true,
-        'enableMinerals' => true,
-        'maxMinerals' => 10,
-        'mineralsAtStart' => true,
-        'enablePlants' => true,
-        'maxPlants' => 8,
-        'plantsAtStart' => true,
-        'enableCrystals' => true,
-        'maxCrystals' => 15,
-        'crystalHealthBonus' => 50,
-        'crystalsAtStart' => true,
-        'enableMeteors' => true,
-        'meteorTicks' => 15,
-        'meteorSpeedMin' => 2,
-        'meteorSpeedMax' => 3,
-        'enableToxicity' => true,
-        'toxicityTicks' => 20,
-        'startToxicity' => 0,
-        'toxicityMin' => 2,
-        'toxicityMax' => 4,
-        'toxicityMultiplier' => 5,
-        'map' => array(
-            "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD",
-            "DDDDDDDDDRRRRRDDDDDDDDDDDRRRRRRDDDDDDDDDDDDD",
-            "DDDDDDDDDDDDDDDRRRRRRRRDDDDDDDDDTTTTTTTTDDDD",
-            "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD",
-            "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD",
-            "DDDDDDDDDDDDDDDDDDDDDDDDTDDDDDDDDDDDDDDDDDDD",
-            "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD",
-            "DDDDDDDDDDDDDDDDDDTDDDDDDDDDDDDDDDDDDDDDDDDD",
-            "DDDDDDDRRRDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD",
-            "DDDDDDDRDRDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD",
-            "DDDDDDDRRRDDDDDTDDDDDDDDDDDDDDDDDDDDDDDDDDDD",
-            "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD",
-            "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD",
-            "DDDDDDDDDDDDDDDDDDTDDDDDDDDDDDDDDDDDDDDDDDDD"
-        ),
-        'nextLevel' => "level2.dat"
-    );
+$ret = LevelGenerator::generate();
 
-    $l = new Level();
-    $l->takeArray($ret);
-
-    $l->addObjective(new CollectObjective("health", "1200"));
-    $l->addObjective(new CollectObjective("crystals", 10));
-    
-    $l->saveToJSON(Config::$defaultLevelFile);
-}
-
-// create level 2.
-if (!file_exists("level2.dat")) {
-    $ret = array();
-
-    $l = new Level();
-    $l->takeArray($ret);
-    $l->saveToJSON("level2.dat");
-}
+$l = new Level();
+$l->takeArray($ret);
+$l->saveToJSON("random.dat");
 
 // GAME START
 echo "\n";
