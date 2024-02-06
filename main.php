53e5726bcfb95bdb0fce5816520a87f9a8070c92
James Wallace <sausages@gmail.com>
2024-02-06 11:44:30 -0500

    fix: cleanup after the autoloader
    Removed the includes that used to be in main.php

--- main.php
+++ main.php
@@ -3,33 +3,6 @@
 // define some system variables
 include 'config.inc';
 
-/*
-include 'wetable.inc';
-include 'treadable.inc';
-
-include 'dirt.inc';
-include 'mailbox.inc';
-include 'rock.inc';
-include 'map.inc';
-include 'player.inc';
-include 'sky.inc';
-include 'plant.inc';
-include 'poi.inc';
-include 'meteor.inc';
-include 'colors.inc';
-include 'config.inc';
-include 'level.inc';
-include 'objective.inc';
-include 'portal.inc';
-include 'levelGenerator.inc';
-include 'power.inc';
-include 'fossil.inc';
-include 'worm.inc';
-include 'pathfinding.inc';
-include 'deer.inc';
-include 'titlescreen.inc';
-*/
-
 // GAME START
 echo "\n";
 
